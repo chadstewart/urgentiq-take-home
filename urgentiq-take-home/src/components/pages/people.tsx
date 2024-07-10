95bf99a24815027681b37e7a5ccb9f9ff1311241
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-10 10:32:09 -0500

    Messing around with main layout css.

--- urgentiq-take-home/src/components/pages/people.tsx
+++ urgentiq-take-home/src/components/pages/people.tsx
@@ -7,12 +7,14 @@ interface PeopleProps {
 
 export default function People({}: PeopleProps) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Test
-      </div>
-    </main>
+    <div className="flex justify-center min-h-screen">
+      <main
+        className={`flex border-l border-r max-w-5xl min-w-[500px] flex-col items-center justify-between p-24`}
+      >
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          Test
+        </div>
+      </main>
+    </div>
   );
 }
