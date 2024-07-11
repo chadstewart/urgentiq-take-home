80796be1782200de0f55b78a3637dec0bc478385
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-11 12:22:38 -0500

    Forgot to add present modal data story.

---
+++ urgentiq-take-home/src/stories/organisms/present-modal-data.stories.tsx
@@ -0,0 +1,57 @@
+import { PresentModalData } from "@/components/organisms/present-modal-data";
+
+const storyConfig = {
+  title: "Design System/Organisms/Present Modal Data",
+};
+
+export default storyConfig;
+
+const testPerson = {
+  name: "Boo",
+  height: "test",
+  mass: "test",
+  hair_color: "test",
+  skin_color: "test",
+  eye_color: "test",
+  birth_year: "test",
+  gender: "test",
+  homeworld: "test",
+  films: ["test"],
+  species: ["test"],
+  vehicles: ["test"],
+  starships: ["test"],
+  created: "test",
+  edited: "test",
+  url: "test",
+};
+
+const testRandomPic = {
+  url: "https://cdn2.thecatapi.com/images/37.gif",
+  width: 499,
+  height: 528,
+};
+
+const testHomeworld = {
+  name: "test",
+  rotation_period: "test",
+  orbital_period: "test",
+  diameter: "test",
+  climate: "test",
+  gravity: "test",
+  terrain: "test",
+  surface_water: "test",
+  population: "test",
+  residents: ["test"],
+  films: ["test"],
+  created: "test",
+  edited: "test",
+  url: "test",
+};
+
+export const PresentModalDataStory = () => (
+  <PresentModalData
+    person={testPerson}
+    randomPic={testRandomPic}
+    homeworld={testHomeworld}
+  />
+);
