152365535d7ddbcbbd8f7d5ea202440fd2a90115
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-14 10:45:54 -0500

    Forgot that some components rely on outputProperDate accepting null values. Put back original null checks.

--- urgentiq-take-home/src/lib/output-proper-date.ts
+++ urgentiq-take-home/src/lib/output-proper-date.ts
@@ -1,5 +1,6 @@
-export const outputProperDate = (dateString: string) => {
+export const outputProperDate = (dateString: string | null) => {
   try {
+    if (!dateString) return "";
     const temp = new Date(dateString);
     if (temp instanceof Date && isNaN(+temp)) return "";
     const formattedDate = `${temp.getDate()}-${temp.getMonth()}-${temp.getFullYear()}`;
